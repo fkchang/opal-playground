89d51301d1f936c2e1d667a07869accdb21e07f5
Forrest Chang <fchang@hedgeye.com>
2015-06-29 16:34:43 -0700

    only add opal-irb as console.log redirector once

--- source/javascripts/result_boot.js.rb
+++ source/javascripts/result_boot.js.rb
@@ -13,6 +13,16 @@ require '_vendor/codemirror-css'
 require '_vendor/codemirror-ruby'
 require '_vendor/codemirror-emacs'
 
+# monkey patched because this gets reloaded everytime we run, opal-irb
+# gets added as a redirected log output. Going on the assumption that
+# we'll only ever add opal-irb.  May change mind w/opal-inspector
+class OpalIrbLogRedirector
+  def self.add_to_redirect redirector
+    initialize_if_necessary
+    @redirectors << redirector if @redirectors.size == 0
+  end
+end
+
 Document.ready? do
   OpalIrbJqconsole.create_bottom_panel
 end
